0cb7cf7f02cf212b972e488f30e3ef9d71cedee0
datajcthemax <data.jcthemax@gmail.com>
2023-08-21 16:40:03 +0900

    DB connected and prints well!!

--- app/main.py
+++ app/main.py
@@ -2,15 +2,31 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+pymysql://admin:qwer1234@outsider-mysql.czxgnu6kme38.ap-northeast-2.rds.amazonaws.com/test"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Table1(Base):
+    __tablename__ = "table1"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
 
 app = FastAPI()
 
-# Static 파일을 위해 마운트 설정
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root(request: Request):
-    analysis_result = "60%"  # 예시 결과
+    db = SessionLocal()  # Create a new session instance
+    analysis_result = db.query(Table1).first().name  # Fetch the name value from the first row
+    db.close()  # Close the session after executing the query
     return templates.TemplateResponse("index.html", {"request": request, "analysis_result": analysis_result})
